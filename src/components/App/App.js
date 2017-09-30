44d6092c744d7c58d1238364ebdc5d1eb1b597c4
Jeremy Marquis <jeremy@jeremymarquis.com>
2017-09-30 00:18:51 -0400

    lotsa spaghetti

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,51 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        heya!
+
+        <nav>
+          <header>
+            <input type="text" />
+          </header>
+          <ul>
+            <li>
+              <a>
+                <div>Upcoming trips</div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Haec et tu ita posuisti, et verba vestra...</p>
+              </a>
+            </li>
+            <li>
+              <a>
+                <div>Upcoming trips</div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Haec et tu ita posuisti, et verba vestra...</p>
+              </a>
+            </li>
+            <li>
+              <a>
+                <div>Upcoming trips</div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Haec et tu ita posuisti, et verba vestra...</p>
+              </a>
+            </li>
+            <li>
+              <a>
+                <div>Upcoming trips</div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Haec et tu ita posuisti, et verba vestra...</p>
+              </a>
+            </li>
+            <li>
+              <a>
+                <div>Upcoming trips</div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Haec et tu ita posuisti, et verba vestra...</p>
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <section>
+          <h2>Words and other things that sound like words</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egone non intellego, quid sit don Graece, Latine voluptas? Universa enim illorum ratione cum tota vestra confligendum puto. Nihil opus est exemplis hoc facere longius. Verum hoc loco sumo verbis his eandem certe vim voluptatis Epicurum nosse quam ceteros. Fortitudinis quaedam praecepta sunt ac paene leges, quae effeminari virum vetant in dolore. Duo Reges: constructio interrete.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quae similitudo in genere etiam humano apparet. Prioris generis est docilitas, memoria; Eodem modo is enim tibi nemo dabit, quod, expetendum sit, id esse laudabile. Non quam nostram quidem, inquit Pomponius iocans; Aliter enim explicari, quod quaeritur, non potest. Tollitur beneficium, tollitur gratia, quae sunt vincla concordiae.</p>
+        </section>
+
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "styles/base"
+
 import React from "react"
 import { render } from "react-dom"
 import { AppContainer } from "react-hot-loader"
