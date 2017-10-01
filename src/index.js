fb5132b9d0698cc854c89cea96a311a1fc884150
Jeremy Marquis <jeremy@jeremymarquis.com>
2017-10-01 00:41:12 -0400

    reduxify me cap'n

--- src/index.js
+++ src/index.js
@@ -4,14 +4,34 @@ import React from "react"
 import { render } from "react-dom"
 import { AppContainer } from "react-hot-loader"
 import { BrowserRouter as Router } from "react-router-dom"
+import { Provider } from "react-redux"
+import { createStore, combineReducers, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
+import { createLogger } from "redux-logger"
 
 import App from "components/App"
 
+import * as reducers from "reducers"
+
+const middleware = [thunk]
+
+if (process.env.NODE_ENV !== "production") {
+  middleware.push(createLogger({
+    level: "info",
+    collapsed: true,
+    diff: true
+  }))
+}
+
+const store = createStore(combineReducers(reducers), applyMiddleware(...middleware))
+
 const renderApp = AppComponent => render(
   <AppContainer>
-    <Router>
-      <AppComponent user="whatever" />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <AppComponent user="whatever" />
+      </Router>
+    </Provider>
   </AppContainer>,
   document.getElementById("root")
 )
