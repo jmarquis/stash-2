3b4f35a89bca8215c4681ee0bf5ff156e72fceb7
Jeremy Marquis <jeremy@jeremymarquis.com>
2017-10-01 21:45:12 -0400

    persistence!

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,7 @@
+import { throttle } from "lodash"
+
+import localData from "etc/localData"
+
 function updateNote(noteId, noteData) {
   return {
     type: "UPDATE_NOTE",
@@ -6,6 +10,13 @@ function updateNote(noteId, noteData) {
   }
 }
 
+function persistContentState(noteId, contentState) {
+  localData.set(`notes.${noteId}.contentState`, contentState)
+}
+
+const throttledPersistContentState = throttle(persistContentState, 500)
+
 export function updateContentState(noteId, contentState) {
+  throttledPersistContentState(noteId, contentState)
   return updateNote(noteId, { contentState })
 }
